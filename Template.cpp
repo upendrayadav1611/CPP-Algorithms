b362d5102f63148134dd3f4b63bc771c8658b915
Upendra Yadav <yadav.11@iitj.ac.in>
2020-04-22 14:44:40 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define ull unsigned long long
+#define ld long double
+
+#define mod 1000000007
+#define EULER 2.7182818284
+#define INF 1000000000
+
+#define pb push_back
+#define ins insert
+#define IT iterator
+#define PQ priority_queue
+
+#define fi first
+#define se second
+#define maxe max_element
+#define mine min_element
+#define er erase
+#define lb lower_bound
+#define ub upper_bound
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+
+
+#define mem(v,i) memset(v,i,sizeof(v)) // v is array. To set all elements to 0 or -1 only. mem(arr,0);
+
+
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORd(i,a,b) for(int i=(a);i>=(b);i--)
+#define FORl(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORN(i,n) for(int i=(0);i<(n);i++)
+#define FORR(i,n) for(int i=(n);i>=0;i--)
+#define bs bitset
+#define bpc(a) __builtin_popcount(a)
+
+ll max(ll a,ll b)
+{
+    if(a>b)return a;return b;
+}
+ll min(ll a,ll b)
+{
+    if(a<b)return a;return b;
+}
+
+ll mypow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+// Get ready to fight!!!!!!
+
+int main()
+{
+    fast
+    
+    return 0;
+}
